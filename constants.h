7650379040d293a0478f01771bceb5905b5adc36
CRaNkXD <crank.xd@outlook.com>
2020-09-01 10:40:24 +0200

    added header constants for storing direction chars

---
+++ constants.h
@@ -0,0 +1,9 @@
+#pragma once
+namespace constants {
+    namespace Direction {
+        constexpr char UP = 'w';
+        constexpr char DOWN = 's';
+        constexpr char RIGHT = 'd';
+        constexpr char LEFT = 'a';
+    }
+}
